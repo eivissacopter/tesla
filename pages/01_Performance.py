8c5d61245e78e4896b83c07753ca6208ac62f946
Julien <95363536+eivissacopter@users.noreply.github.com>
2024-08-07 22:50:52 +0200

    Update 01_Performance.py

--- pages/01_Performance.py
+++ pages/01_Performance.py
@@ -265,6 +265,37 @@ show_legend = st.sidebar.checkbox("Show Legend", value=False)
 
 ####################################################################################################
 
+# Add custom CSS to control the layout
+st.markdown(
+    """
+    <style>
+    .main .block-container {
+        padding-top: 0rem;
+        padding-right: 1rem;
+        padding-left: 1rem;
+        padding-bottom: 0rem;
+    }
+    .main {
+        display: flex;
+        flex-direction: column;
+        height: 100vh;
+        overflow: hidden;
+    }
+    .element-container {
+        flex: 1;
+        height: 100%;
+    }
+    .stPlotlyChart {
+        height: 100% !important;
+    }
+    button[title="View fullscreen"] {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 # Plotting the data using Plotly
 if selected_x_axis and selected_columns and filtered_file_info:
     plot_data = []
@@ -311,24 +342,17 @@ if selected_x_axis and selected_columns and filtered_file_info:
             xaxis_title=selected_x_axis,
             yaxis_title="Values" if len(selected_columns) > 1 else selected_columns[0],
             autosize=True,
-            height=None
-        )
-        if show_legend:
-            fig.update_layout(showlegend=True)
-        else:
-            fig.update_layout(showlegend=False)
-
-        # Set the Plotly chart to be responsive
-        fig.update_layout(
+            height=None,
             margin=dict(l=20, r=20, t=30, b=20),
-            modebar=dict(orientation='v'),
-            autosize=True,
-            height=None
+            modebar=dict(orientation='v')
         )
+        fig.update_yaxes(automargin=True)
+        fig.update_xaxes(automargin=True)
 
-        st.plotly_chart(fig, use_container_width=True)
+        st.plotly_chart(fig, use_container_width=True, config={'scrollZoom': True, 'displayModeBar': True, 'responsive': True, 'toImageButtonOptions': {'format': 'svg', 'filename': 'custom_image', 'height': None, 'width': None, 'scale': 1}})
 else:
     st.write("Please select an X-axis and at least one column to plot.")
+
 
 
 
