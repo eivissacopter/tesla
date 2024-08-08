f946c4161653b979ec20a5a201ab303e353811f3
Julien <95363536+eivissacopter@users.noreply.github.com>
2024-08-08 10:47:25 +0200

    Update 01_Performance.py

--- pages/01_Performance.py
+++ pages/01_Performance.py
@@ -274,6 +274,10 @@ show_legend = st.sidebar.checkbox("Show Legend", value=True)
 
 ####################################################################################################
 
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import matplotlib.colors as mcolors
+
 # Plotting the data using Plotly
 if selected_x_axis and selected_columns and filtered_file_info:
     plot_data = []
@@ -361,18 +365,17 @@ if selected_x_axis and selected_columns and filtered_file_info:
             xaxis_title=selected_x_axis,
             yaxis_title="Values" if len(selected_columns) > 1 else selected_columns[0],
             width=800,  # Adjust width as needed
-            height=600,  # Adjust height as needed
-            margin=dict(l=0, r=300, t=50, b=50),  # Add margin to the right for legend
+            height=800,  # Adjust height as needed
+            margin=dict(l=50, r=50, t=50, b=150),  # Add margin to the bottom for legend
             legend=dict(
-                orientation="v",  # Vertical legend
+                orientation="h",  # Horizontal legend
                 yanchor="top",
-                y=1,
-                xanchor="left",
-                x=1.05  # Adjust position of legend outside of the plot
+                y=-0.3,  # Position the legend below the plot
+                xanchor="center",
+                x=0.5
             )
         )
 
         st.plotly_chart(fig, use_container_width=True)
 else:
     st.write("Please select an X-axis and at least one column to plot.")
-
